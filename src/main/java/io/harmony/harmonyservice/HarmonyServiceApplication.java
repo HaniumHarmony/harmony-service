007770ca29f7096f0873f21fd2e3ea3a60cd2c69
nanami-tomoe <npnp123npnp123@gmail.com>
2024-08-07 22:57:42 +0900

    initial commit

---
+++ src/main/java/io/harmony/harmonyservice/HarmonyServiceApplication.java
@@ -0,0 +1,13 @@
+package io.harmony.harmonyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarmonyServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/harmony/harmonyservice/HarmonyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.harmony.harmonyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
